d1d21ccacdc03f7f2e41ea1bd727b3913316bfcb
PasqualeIannace <pasquale.iannace@gmail.com>
2023-12-07 17:18:49 +0100

    inserico la mezza stella

--- src/components/Section1.vue
+++ src/components/Section1.vue
@@ -13,24 +13,57 @@ export default {
             event.target.src = "/unknownPoster.png";
         },
 
-        fixVoto(voto) {
+        debugVoto(voto) {
+            voto = voto.toFixed() / 2;
 
-            if (voto == null) {
-                console.log("Voto vale NULL", voto);
+            return voto;
+        },
+
+
+        intVoto(voto) {
+            if (voto == 0) {
                 return 0;
             };
 
             voto = voto.toFixed() / 2;
 
             if (Number.isInteger(voto) == false) {
-                console.log("Voto è mezzo numero e vale: ", voto);
+                //console.log("Voto è mezzo numero e vale: ", voto);
                 voto = parseInt(voto);
-                console.log("Voto è stato convertito in intero", voto);
+                //console.log("Voto è stato convertito in intero", voto);
                 return voto;
             };
 
             return voto
         },
+
+        halfStar(voto) {
+            voto = voto.toFixed() / 2;
+
+            if (Number.isInteger(voto) == false) {
+                voto = 1;
+                return voto;
+            }
+        },
+
+        zeroStar(voto) {
+            //console.log("Sei in zeroStar e voto vale:", voto);
+
+            if (voto == 0) {
+                return 5;
+            };
+
+            voto = voto.toFixed() / 2;
+
+            if (Number.isInteger(voto) == false) {
+                let NewVoto = parseInt(voto);
+                console.log("Voto in zero star vale: ", voto);
+                voto = NewVoto - 4;
+                voto = Math.abs(voto);
+                console.log("Voto in zero star dopo sottrazione: ", voto);
+                return voto;
+            };
+        }
     },
 
     mounted() {
@@ -51,9 +84,11 @@ export default {
             <h2>{{ film.title }}</h2>
             <h3>{{ film.original_title }}</h3>
             <img @error="imgError" class="flags" :src="`/flags/${film.original_language}.png`" alt="">
-            <p class="py1">Voto:
+            <p class="py1">Voto numerico: {{ debugVoto(film.vote_average) }}
             </p>
-            <img class="stars" v-for="i in fixVoto(film.vote_average)" src="/stars/star.png" alt="">
+            <img class="stars" v-for="i in intVoto(film.vote_average)" src="/stars/star.png" alt="">
+            <img class="stars" v-for="i in halfStar(film.vote_average)" src="/stars/halfStar.png" alt="">
+            <img class="stars" v-for="i in zeroStar(film.vote_average)" src="/stars/emptyStar.png" alt="">
             <span>{{ film.overview }}</span>
         </div>
 
